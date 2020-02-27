487c02e0458c0692b5e1331d38bda791039ad10c
Ezzara Estell <eestell@at-k7.21-school.ru>
2020-02-27 10:58:23 +0300

    ft_printf_27_02_20(2)

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: eestell <eestell@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 20:27:20 by eestell           #+#    #+#             */
-/*   Updated: 2020/02/27 10:16:41 by eestell          ###   ########.fr       */
+/*   Updated: 2020/02/27 10:57:50 by eestell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,39 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void	ft_printf(char *in_print, ...)
+void	ft_converttoformat(char *format, char *str);
+{
+	
+}
+void	ft_printf(char *trav, ...)
 {
 	va_list		argv;
-	char		*trav = NULL;
 	char		*s;
 	char		*str;
 	int			i;
 
 	i = 0;
-	trav = in_print;
-	va_start(argv, in_print);
-	//puts(in_print);
-	str = ft_strdup(in_print);
-	//ft_find_format(str);
-	while (trav != '\0' || s == NULL) /*you must test number of %*/
+	va_start(argv, trav);
+	str = ft_strdup(trav);
+
+	while (trav) /*you must test number of %*/
 	{
-		while (str[i] != '%')
+
+		s = va_arg(argv, char*);
+		if (trav == '\0' || s == NULL)
+			break;
+		while (str[i] != '%'  && trav != '\0')
 		{
-			write(1, &str[i], 1);
+			if (str[i - 1] != '%')
+				write(1, &str[i], 1);
 			i++;
 		}
 		i++;
 		printf("\n\n");
-		s = va_arg(argv, char*);
-		if (trav == '\0' || s == NULL)
-		{
-			va_end(argv);
-			break;
-		}
 		ft_putstr(s);
 		ft_putchar('\n');
-		trav++;
 	}
+	va_end(argv);
 }
 
 
